cd3cbbe200a2a33742d5b2c787b67b7c04123327
Matheus Henrique Mariano <141952781+devnonimate@users.noreply.github.com>
2023-11-27 13:49:43 -0300

    Clonegram

---
+++ clonegram.py
@@ -0,0 +1,83 @@
+from telethon import TelegramClient, events
+import asyncio
+from telethon.tl.functions.channels import ReadHistoryRequest
+from telethon.sessions import MemorySession
+import re
+import datetime
+
+ORIGINAL_CHANNEL_USERNAME = 'sbroletabrasileira'  # Substitua pelo nome do canal original
+NOVO_CANAL_USERNAME = 'greenbetsrb'  # Substitua pelo nome do novo canal
+SESSION_FILE = MemorySession()
+
+def iniciar_cliente():
+    client = TelegramClient(SESSION_FILE, 25356283, 'efcf5005566af49047f19ccc708ed371')
+    return client
+
+async def main():
+    client = iniciar_cliente()
+    await client.start()
+    original_channel = await client.get_entity(ORIGINAL_CHANNEL_USERNAME)
+    novo_canal = await client.get_entity(NOVO_CANAL_USERNAME)
+
+    mensagens_clonadas = 0
+    green_count = 0
+    red_count = 0
+    is_clone_paused = False
+    mensagem_anterior = None
+
+    @client.on(events.NewMessage(chats=original_channel))
+    async def handler(event):
+        nonlocal mensagens_clonadas, green_count, red_count, is_clone_paused, mensagem_anterior
+
+        if is_clone_paused:
+            return
+
+        if mensagem_anterior and event.text == mensagem_anterior.text:
+            # Apaga a mensagem duplicada no novo canal
+            await client.delete_messages(novo_canal, [event.message.id])
+        else:
+            mensagem_anterior = event.message
+
+            if "ANALISANDO" in event.text or "ENTRADA CONFIRMADA" in event.text or "APOSTA ENCERRADA" in event.text:
+                modified_text = modify_message_link(event.message)
+                await client.send_message(novo_canal, modified_text)
+                if "APOSTA ENCERRADA" in event.text:
+                    mensagens_clonadas += 1
+                    if "GREEN" in event.text:
+                        green_count += 1
+                    elif "RED" in event.text:
+                        red_count += 1
+
+        if mensagens_clonadas == 8:
+            await client.send_message(novo_canal, f'RELATÓRIO:\nGREEN {green_count} ✅\nRED {red_count} ❌')
+            mensagens_clonadas = 0
+            green_count = 0
+            red_count = 0
+            is_clone_paused = True
+
+    async def resume_cloning():
+        nonlocal is_clone_paused
+        while True:
+            now = datetime.datetime.now().time()
+            if now.hour == 11 and now.minute == 0:
+                is_clone_paused = False
+            elif now.hour == 16 and now.minute == 0:
+                is_clone_paused = False
+            elif now.hour == 20 and now.minute == 0:
+                is_clone_paused = False
+            await asyncio.sleep(60)  # Verifica a cada minuto
+
+    asyncio.ensure_future(resume_cloning())
+    await client.run_until_disconnected()
+
+def modify_message_link(message):
+    if message.text:
+        modified_text = re.sub(r'Playtech - Roleta Brasileira', 'GreenBets - Roleta Brasileira', message.text)
+        modified_text = re.sub(r'https://www.segurobet.com/\?btag=1176229&accounts=%2A&register=%2A', 'https://afiliados.greenbets.io/visit/?bta=62357&brand=greenbetsio', modified_text)
+        return modified_text
+    else:
+        return ""
+
+asyncio.run(main())
+
+#88466857
